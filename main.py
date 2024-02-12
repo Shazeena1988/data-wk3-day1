97005d87e3ebf81b6b402602ef5c1cadd7c5b2b5
Shazeena Hussain <shazeena1988@gmail.com>
2024-02-12 10:56:11 +0000

    adding self

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from person import Person
 
-liam = Person("Liam", 30, "Tall")
+shaz = Person("shaz", 35, "160")
+
+print(shaz.name)
+print(shaz.age)
+print(shaz.height)
--- person.py
+++ person.py
@@ -1,3 +1,5 @@
 class Person():
-    def __innit__(self, person_name):
+    def __innit__(self, person_name, person_age, person_height):
         self.name = person_name
+        self.age = person_age
+        self.height = person_height
