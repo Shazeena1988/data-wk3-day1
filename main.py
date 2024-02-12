5a409a5ed7c0960343bbe4f7a8131e94e7660fdb
Shazeena Hussain <shazeena1988@gmail.com>
2024-02-12 13:35:29 +0000

    updated superheroes

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from superhero import Superhero
 
 superman = Superhero("Superman", "Clark Kent" , "Man of Steel", "Lex Luthor")
-spiderman = Superhero ("Spiderman", "Peter Parker", "Spidey Sense", "The Green Goblin")
+spiderman = Superhero ("Spiderman", "Peter Parker", "Spidey Senses", "The Green Goblin")
 ironman = Superhero ("Iron Man", "Tony Stark", "Armour", "The Mandarin")
-batman = Superhero ("Batman", "Bruce Wayne", "Strong" "The Joker")
+batman = Superhero ("Batman", "Bruce Wayne", "Strong", "The Joker")
 
-print(f"My superhero name is {superman.name} and secret identity is {superman.identity} my superpower is I am {superman.power} and my arch enemy is {superman.enemy}")
+print(f"I am {superman.name} my real name is {superman.identity} my superpower is I am the {superman.power} my arch enemy is {superman.enemy}")
+print(f"I am {spiderman.name} my real name is {spiderman.identity} my superpower is I have {spiderman.power} my arch enemy is {spiderman.enemy}")
+print(f"I am {ironman.name} my real name is {ironman.identity} my superpower is I have {ironman.power} my arch enemy is {ironman.enemy}")
+print(f"I am {batman.name} my real name is {batman.identity} my superpower is I am {batman.power} my arch enemy is {batman.enemy}")
--- superhero.py
+++ superhero.py
@@ -1,5 +1,5 @@
 class Superhero():
-    def __innit__(self, superhero_name, superhero_identity, superhero_power, superhero_enemy):
+    def __init__(self, superhero_name, superhero_identity, superhero_power, superhero_enemy):
         self.name = superhero_name
         self.identity = superhero_identity
         self.power = superhero_power
